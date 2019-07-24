c97fc05da26750dc3c29db3577fad3d11aa85645
Fridolin Adenito <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSexAkgCLo3eIFuZYLlpGMBoAQFzRWGoJOXGT5kePY9X3D2E1vDJF+TwudOwibTYY6lZ48SyigEKGyoo0ayztFAABF2oDu4H6Rmj2FAWUDe23oC3+/vCy1YohoHlG5vjY89es6HmUw6cNwF3YZygbs5SxxhK2KmhcBqtUmmrxpTT8ZjuRdM5aWA3wiotEJ5xXujRKsBMksntpIURoAnhwbWvEbZnv13lP6x//THrUYJbO2nmTGcYYfPIDXtRkK5LvczoHD2A/fLr7Yq2VAXaC7C3/LF9eK50NkpmWpIpazIdfkFNIGIkC43iG+mE7VsGYT+0HZVv6ONFah6HhWVl8J faefi@DESKTOP-9TIE2SV>
2019-07-24 12:01:47 +0700

    fix bukti

--- resources/views/transaksi/detailPembayaran.blade.php
+++ resources/views/transaksi/detailPembayaran.blade.php
@@ -39,7 +39,7 @@
 		</table>
 	</div>
 	<div class="col-md-6">
-		<?php echo '<img width="100%" class="img-responsive" src="../BuktiPembayaran/'.$pecah->Bukti_Pembayaran.'" alt="">' ?>
+		<?php echo '<img width="100%" class="img-responsive" src="BuktiPembayaran/'.$pecah->Bukti_Pembayaran.'" alt="">' ?>
 	</div>
 </div>
 
