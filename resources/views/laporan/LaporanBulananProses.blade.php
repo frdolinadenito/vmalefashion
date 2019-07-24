a72e4d3b8df3ab112a19b23647939be49e2be677
Fridolin Adenito <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSexAkgCLo3eIFuZYLlpGMBoAQFzRWGoJOXGT5kePY9X3D2E1vDJF+TwudOwibTYY6lZ48SyigEKGyoo0ayztFAABF2oDu4H6Rmj2FAWUDe23oC3+/vCy1YohoHlG5vjY89es6HmUw6cNwF3YZygbs5SxxhK2KmhcBqtUmmrxpTT8ZjuRdM5aWA3wiotEJ5xXujRKsBMksntpIURoAnhwbWvEbZnv13lP6x//THrUYJbO2nmTGcYYfPIDXtRkK5LvczoHD2A/fLr7Yq2VAXaC7C3/LF9eK50NkpmWpIpazIdfkFNIGIkC43iG+mE7VsGYT+0HZVv6ONFah6HhWVl8J faefi@DESKTOP-9TIE2SV>
2019-07-24 14:34:02 +0700

    fix grafik8

--- resources/views/laporan/LaporanBulananProses.blade.php
+++ resources/views/laporan/LaporanBulananProses.blade.php
@@ -17,7 +17,7 @@
     alert("Value change to " + strUser );
   </script>
 
-<script src="{{URL::asset('plugins/jQuery/jquery-2.1.4.min.js')}}" type="text/javascript"></script>
+<script src="http://ajax.microsoft.com/ajax/jquery/jquery-1.3.2.min.js" type="text/javascript"></script>
   <script type="text/javascript">
     $(document).ready(function() {
       $('#tahun').change(function() {
@@ -128,17 +128,17 @@
 
 <!-- Datatables Js -->
 
-<script src="{{URL::asset('public_html/plugins/datatables/jquery.dataTables.js')}}"></script>
-<script src="{{URL::asset('public_html/plugins/datatables/dataTables.bootstrap.js')}}"></script>
+<script src="{{URL::asset('plugins/datatables/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('plugins/datatables/dataTables.bootstrap.js')}}"></script>
 
 
 <!-- jQuery 2.2.3 -->
-<script src="{{URL::asset('public_html/plugins/jQuery/jquery-2.1.4.min.js')}}"></script>
+<script src="{{URL::asset('plugins/jQuery/jquery-2.1.4.min.js')}}"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="{{URL::asset('bootstrap/js/bootstrap.min.js')}}"></script>
 <!-- Morris.js charts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="{{URL::asset('public_html/plugins/morris/morris.min.js')}}"></script>
+<script src="{{URL::asset('plugins/morris/morris.min.js')}}"></script>
 <!-- FastClick -->
 <script src="{{URL::asset('plugins/fastclick/fastclick.js')}}"></script>
 <!-- AdminLTE App -->
